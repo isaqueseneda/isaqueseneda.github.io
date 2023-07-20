49880c23195076ad20883a5ee83e1c2349a253c1
Isaque Seneda <73655063+isaqueseneda@users.noreply.github.com>
2023-07-20 19:43:21 +0200

    chaos2

--- js/webflow.js
+++ js/webflow.js
@@ -10,10 +10,22 @@ audioElement.loop = true;
 const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
+
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
+
   // 3. Play the audio
   audioElement.play();
+
+  // 4. Add distortions
+  distortElements();
+
+  // 5. Change background
+    // Add 'background-active' class to the body
+    document.body.classList.add('background-active');
+  
+    // Skew the background
+    skewBackground();
 }
 
 const disableDarkMode = () => {
@@ -21,12 +33,19 @@ const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
+
   // 3. Pause the audio
   audioElement.pause();
   // 4. Reset the audio time
   audioElement.currentTime = 0;
+  
+  // 5. Remove all distortions
+  revertElements();
+
+  // Remove 'background-active' class from the body
+  document.body.classList.remove('background-active');
 }
- 
+
 // If the user already visited and enabled darkMode
 // start things off with it on
 if (darkMode === 'enabled') {
@@ -54,3 +73,58 @@ window.addEventListener('load', () => {
     audioElement.play();
   }
 });
+
+// chaos distort function
+function distortElements() {
+  // Get all elements on the page
+  let allElements = document.querySelectorAll('*');
+
+  // Iterate over each element
+  allElements.forEach((el) => {
+    // Ignore elements with tagName 'HTML', 'BODY' or class 'inheadertitlebox' or 'header'
+    if (el.tagName === 'HTML' || el.tagName === 'BODY' || el.classList.contains('inheadertitlebox') || el.classList.contains('header') || el.classList.contains('theproject')) {
+      return;
+    }
+
+    // Generate random skew and scale values
+    let skewX = Math.random() * 60 - 30; // Between -30 and 30 degrees
+    let skewY = Math.random() * 60 - 30; // Between -30 and 30 degrees
+    let scaleX = 1 + (Math.random() * 0.2 - 0.1); // Between 0.9 and 1.1
+    let scaleY = 1 + (Math.random() * 0.2 - 0.1); // Between 0.9 and 1.1
+
+    // Set the transformation
+    el.style.transform = `skew(${skewX}deg, ${skewY}deg) scale(${scaleX}, ${scaleY})`;
+  });
+}
+
+// Remove all transformations
+function revertElements() {
+  // Get all elements on the page
+  let allElements = document.querySelectorAll('*');
+
+  // Iterate over each element
+  allElements.forEach((el) => {
+    // Ignore elements with tagName 'HTML', 'BODY' or class 'inheadertitlebox' or 'header'
+    if (el.tagName === 'HTML' || el.tagName === 'BODY' || el.classList.contains('inheadertitlebox') || el.classList.contains('header') || el.classList.contains('theproject')) {
+      return;
+    }
+
+    // Remove all transformations
+    el.style.transform = '';
+  });
+}
+
+
+
+
+
+//skewing-bg
+function skewBackground() {
+  // Generate random skew values
+  let skewX = Math.random() * 20 + 70; // Between -60 and 60 degrees
+  let skewY = Math.random() * 5 + 10; // Between -60 and 60 degrees
+
+  // Apply skew to the body's before pseudo-element
+  document.documentElement.style.setProperty('--bg-skew-x', `${skewX}deg`);
+  document.documentElement.style.setProperty('--bg-skew-y', `${skewY}deg`);
+}
